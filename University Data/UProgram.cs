d8cf4b5d4c8afdb7fff39f06f6c82cd1eb8ed500
Jayant Arora <jaistudy1996@gmail.com>
2017-10-06 21:54:18 -0400

    add comments

--- University Data/UProgram.cs
+++ University Data/UProgram.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 
 namespace UPrograms{
     class UProgram{
+        // using list class from system collection generic
         public List<Degree> Degree{get; set;}
         public string Name{get; set;}
         public UProgram(string name, List<Degree> degrees){
